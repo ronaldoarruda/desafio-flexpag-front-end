e19c781baea9a7fd18f40b43d25462ffb86fe587
Ronaldo Arruda <nronaldo_7@hotmail.com>
2022-07-30 00:12:57 -0300

    rota, usabilidade e estilização scroll bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { CurrencyCotationComponent } from './pages/currency-cotation/currency-cotation.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:CurrencyCotationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
